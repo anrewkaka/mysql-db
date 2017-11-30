3cc8fb7f279c3e1d0549f949acd6d1bb2a34b56d
anrewkaka <mr.anrew@gmail.com>
2017-11-30 18:24:24 +0900

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -6,16 +6,11 @@ fi
 # mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
 mount -o defaults /dev/sdb /data
 
-if [[ ! -d /opt/mysql-db ]]; then
-  git clone https://github.com/anrewkaka/mysql-db.git /opt/mysql-db
-fi
-
 # docker restart
 sed -i 's/dockerd -H fd\:\/\/$/dockerd -H fd\:\/\/ --bip=10.0.42.1\/24 --fixed-cidr=10.0.42.0\/24/' /lib/systemd/system/docker.service
 systemctl daemon-reload
 systemctl restart docker
 
-cd /opt/subversion
 git reset --hard
 git clean -f
 git checkout master
