157f63d737d8ead0a34044bd6a6bbbb5d3cffc42
anrewkaka <mr.anrew@gmail.com>
2017-12-01 11:06:04 +0900

    Update config.user.inc.php

--- conf/config.user.inc.php
+++ conf/config.user.inc.php
@@ -25,7 +25,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = 'mysql-db';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 /**
