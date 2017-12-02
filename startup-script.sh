ed55fbff2f53104d71656a9ef916a493cd0f59d6
anrewkaka <mr.anrew@gmail.com>
2017-12-02 10:23:54 +0700

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -3,12 +3,9 @@
 if [ ! -d "/data" ]; then
   mkdir -p /data
 fi
-if [ ! -d "/phpmyadmin" ]; then
-  mkdir -p /phpmyadmin
-fi
 
 # mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
-mount -o defaults /dev/sdb /data
+mount /dev/sdb /data
 
 # docker restart
 sed -i 's/dockerd -H fd\:\/\/$/dockerd -H fd\:\/\/ --bip=10.0.42.1\/24 --fixed-cidr=10.0.42.0\/24/' /lib/systemd/system/docker.service
